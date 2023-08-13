435603ac989dd76b0794b03d6e376b3be770801d
mErenSekkeli <erensekkeli46@gmail.com>
2023-08-13 12:16:17 +0300

    service and repo improvements using specifications

--- src/main/java/com/hoaxify/webservice/hoax/HoaxRepository.java
+++ src/main/java/com/hoaxify/webservice/hoax/HoaxRepository.java
@@ -3,18 +3,27 @@ package com.hoaxify.webservice.hoax;
 import com.hoaxify.webservice.user.Users;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface HoaxRepository extends JpaRepository<Hoaxes, Long> {
+import java.util.List;
+
+public interface HoaxRepository extends JpaRepository<Hoaxes, Long>, JpaSpecificationExecutor<Hoaxes> {
 
     Page<Hoaxes> findByUser(Users user, Pageable page);
 
-    Page<Hoaxes> findByIdLessThan(long id, Pageable page);
+//    Page<Hoaxes> findByIdLessThan(long id, Pageable page);
 
-    Page<Hoaxes> findByIdLessThanAndUser(long id, Users user, Pageable page);
+//    Page<Hoaxes> findByIdLessThanAndUser(long id, Users user, Pageable page);
 
-    long countByIdGreaterThan(long id);
+//    long countByIdGreaterThan(long id);
 
-    long countByIdGreaterThanAndUser(long id, Users user);
+//    long countByIdGreaterThanAndUser(long id, Users user);
+
+    List<Hoaxes> findTop10ByIdGreaterThan(long id, Sort sort);
+
+    List<Hoaxes> findTop10ByIdGreaterThanAndUser(long id, Users user, Sort sort);
 
 }
--- src/main/java/com/hoaxify/webservice/hoax/HoaxService.java
+++ src/main/java/com/hoaxify/webservice/hoax/HoaxService.java
@@ -5,7 +5,11 @@ import com.hoaxify.webservice.user.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HoaxService {
@@ -26,8 +30,14 @@ public class HoaxService {
         return hoaxRepository.findAll(page);
     }
 
-    public Page<Hoaxes> getOldHoaxes(long id, Pageable page) {
-        return hoaxRepository.findByIdLessThan(id, page);
+    public Page<Hoaxes> getOldHoaxes(long id, String username, Pageable page) {
+        Specification<Hoaxes> specification = Specification.where(idLessThan(id));
+        if(username != null){
+            Users inDB = userService.getUserByUserName(username);
+            specification = specification.and(userIs(inDB));
+            return hoaxRepository.findAll(specification, page);
+        }
+        return hoaxRepository.findAll(specification, page);
     }
 
     public Page<Hoaxes> getHoaxesOfUser(String username, Pageable page){
@@ -35,17 +45,33 @@ public class HoaxService {
         return hoaxRepository.findByUser(inDB, page);
     }
 
-    public Page<Hoaxes> getOldHoaxesOfUser(long id, String username, Pageable page){
-        Users inDB = userService.getUserByUserName(username);
-        return hoaxRepository.findByIdLessThanAndUser(id, inDB, page);
-    }
-
     public long getNewHoaxesCount(long id, String username) {
+        Specification<Hoaxes> specification = Specification.where(idGreaterThan(id));
         if(username != null){
             Users inDB = userService.getUserByUserName(username);
-            return hoaxRepository.countByIdGreaterThanAndUser(id, inDB);
+            specification = specification.and(userIs(inDB));
         }
-        return hoaxRepository.countByIdGreaterThan(id);
+        return hoaxRepository.count(specification);
+    }
+
+    public List<Hoaxes> getNewHoaxes(long id, String username, Sort sort) {
+        if(username != null){
+            Users inDB = userService.getUserByUserName(username);
+            return hoaxRepository.findTop10ByIdGreaterThanAndUser(id, inDB, sort);
+        }
+        return hoaxRepository.findTop10ByIdGreaterThan(id, sort);
+    }
+
+    private Specification<Hoaxes> idLessThan(long id){
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThan(root.get("id"), id);
+    }
+
+    private Specification<Hoaxes> userIs(Users user){
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("user"), user);
+    }
+
+    private Specification<Hoaxes> idGreaterThan(long id){
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("id"), id);
     }
 
 }
